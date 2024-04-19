f22d8e92f6498e2991caf94b9d2b5c9b29cb559f
Zakhar Polikarpov <zpolikarpov@veka.de>
2024-04-19 11:59:39 +0200

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react"
+import img_drawers from "./assets/images/drawers.jpg"
+
 function App() {
 	return (
 		<>
-			
+			<main>
+				<div className="container">
+					<div className="card">
+						<img src={img_drawers} alt="" />
+						<div className="card__body">
+							<h1 className="card__heading">Shift the overall look and feel by adding these wonderful touches to furniture in your home</h1>
+							<p>Ever been in a room and felt like something was missing? Perhaps it felt slightly bare and uninviting. I’ve got some simple tips to help you make any room feel complete.</p>
+							<div className="card__footer"></div>
+						</div>
+					</div>
+				</div>
+			</main>
 		</>
   )
 }
